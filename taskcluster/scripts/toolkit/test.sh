737353fead2b396166472ae907ff9126830fe52e
Sebastian Streich <sstreich@mozilla.com>
2022-02-09 16:40:06 +0100

    attempt 1

--- taskcluster/scripts/toolkit/test.sh
+++ taskcluster/scripts/toolkit/test.sh
@@ -4,26 +4,18 @@ set -e
 
 # Dependencies
 ##python3 scripts/tooltool.py --url http://taskcluster/tooltool.mozilla-releng.net fetch -m windows/qt-dependencies.tt.manifest
+mkdir zip && cd zip
+wget -q https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/EAtDCYYTTXaY4p3JDwovLQ/runs/0/artifacts/public/qt_src.zip || true
+wget -q https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/EnZvH5r1RjKVcGtk6SUbjQ/artifacts/public/vsstudio.zip|| true
+unzip -o -qq vsstudio.zip|| true
+unzip -o -qq qt_src.zip|| true
 
 
-python3 -m pip list
-python3 -m pip show certifi
-python3 -m certifi
-
-echo "---"
 ls -ial
 
+
+echo "---"
+cd ../
 echo "---"
 
 python3 taskcluster/scripts/toolkit/test.py
-
-
-
-
-python3 scripts/tooltool.py --url http://taskcluster/tooltool.mozilla-releng.net fetch -m taskcluster/manifests/vs2017-15.9.manifest
-cd fetch
-unzip vs2017_15.9.6.zip
-cd ..
-./taskcluster/scripts/misc/vs-setup.sh
-
-where msbuild
