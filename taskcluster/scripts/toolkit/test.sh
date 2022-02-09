cd2d829651677e7dfe2f3c63755ac7cb067a8a3b
Sebastian Streich <sstreich@mozilla.com>
2022-02-09 18:24:26 +0100

    more testing

--- taskcluster/scripts/toolkit/test.sh
+++ taskcluster/scripts/toolkit/test.sh
@@ -4,18 +4,16 @@ set -e
 
 # Dependencies
 ##python3 scripts/tooltool.py --url http://taskcluster/tooltool.mozilla-releng.net fetch -m windows/qt-dependencies.tt.manifest
-mkdir zip && cd zip
+mkdir fetches && cd fetches
 wget -q https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/EAtDCYYTTXaY4p3JDwovLQ/runs/0/artifacts/public/qt_src.zip || true
 wget -q https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/EnZvH5r1RjKVcGtk6SUbjQ/artifacts/public/vsstudio.zip|| true
 unzip -o -qq vsstudio.zip|| true
 unzip -o -qq qt_src.zip|| true
-
-
 ls -ial
-
 
 echo "---"
 cd ../
 echo "---"
+ps -ExecutionPolicy Bypass taskcluster/scripts/toolchain/compile_qt_6.ps1   
 
 python3 taskcluster/scripts/toolkit/test.py
